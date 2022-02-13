c1a40aa73bba60977f5db3e25346d8f94e0fcb71
alex <strim0125@yandex.ru>
2022-02-13 14:57:26 +0300

    1.1

--- src/ru/iegs/p2_4/Main.java
+++ src/ru/iegs/p2_4/Main.java
@@ -1,8 +1,17 @@
 package ru.iegs.p2_4;
 
-public class Main {
+import javax.swing.*;
+
+class MyWindow extends JFrame {
+    public MyWindow() {
+        setTitle("Test Window");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 400);
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
-	    
+        new MyWindow();
     }
 }
+
